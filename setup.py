636b1570986f1883b9a479be61aaef4fd6f37aed
Paul Scruby <paul@blackbox.tech>
2021-01-21 22:53:19 +0000

    Fixed setup script:

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from pathlib import Path
 
 setup(
     name="conda_test",
-    version="1.0.5",
+    version="1.0.6",
     package_dir={"": "lib"},
     packages=find_namespace_packages(where="lib"),
-    data_files=[(dir, [str(fn) for fn in Path(dir).glob("*") if fn.is_file()]) for dir in Path("share").glob("**/*") if dir.is_dir()],
+    data_files=[(str(dir), [str(fn) for fn in Path(dir).glob("*") if fn.is_file()]) for dir in Path("share").glob("**/*") if dir.is_dir()],
     scripts=[str(fn) for fn in Path("scripts").glob("**/*") if fn.is_file()],
     include_package_data=True,
     zip_safe=False,
